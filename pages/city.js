f45916f2ac92d3dd581bc9ff60ac7e6701cc5569
dhaniald <gtiwari95@gmail.com>
2023-09-27 16:07:42 +0530

    c5

--- pages/city.js
+++ pages/city.js
@@ -4,7 +4,7 @@ import React from 'react'
 //defenation area
 export default function city() {
   return (
-    <section className='col-6 g_tbdr'>
+    <section className="col-6 g_tbdr">
         <h1>The City for Next Js</h1>
         <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form.</p>
     </section>
